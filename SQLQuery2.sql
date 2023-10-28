4f059ef910da6dc92604e4fb15dbec574b7635d0
andre graes <andregraes@gmail.com>
2023-10-28 15:23:28 +0100

    desafio resolvido

---
+++ SQLQuery2.sql
@@ -0,0 +1,76 @@
+--1
+SELECT
+	nome,
+	ano
+FROM Filmes
+
+--2
+SELECT Nome, Ano
+FROM Filmes
+ORDER BY Ano ASC;
+
+--3
+SELECT Nome, Ano, Duracao
+FROM Filmes
+WHERE Nome = 'De Volta para o Futuro';
+
+
+--4
+SELECT Nome, Ano
+FROM Filmes
+WHERE Ano = 1997;
+
+--5
+SELECT Nome, Ano
+FROM Filmes
+WHERE Ano > 2000;
+
+
+--6
+SELECT Nome, Duracao
+FROM Filmes
+WHERE Duracao > 100 AND Duracao < 150
+ORDER BY Duracao ASC;
+
+
+--7
+SELECT Ano, COUNT(*) AS QuantidadeDeFilmes
+FROM Filmes
+GROUP BY Ano
+ORDER BY QuantidadeDeFilmes DESC;
+
+
+--8
+SELECT PrimeiroNome, UltimoNome, Genero
+FROM Atores
+WHERE Genero = 'M';
+
+
+--9
+SELECT PrimeiroNome, UltimoNome
+FROM Atores
+WHERE Genero = 'F'
+ORDER BY PrimeiroNome;
+
+
+--10
+SELECT F.Nome AS NomeDoFilme, G.Genero
+FROM Filmes AS F
+JOIN FilmesGenero AS FG ON F.Id = FG.IdFilme
+JOIN Generos AS G ON FG.IdGenero = G.Id
+ORDER BY G.Genero;
+
+
+--11
+SELECT F.Nome AS NomeDoFilme, G.Genero
+FROM Filmes AS F
+JOIN FilmesGenero AS FG ON F.Id = FG.IdFilme
+JOIN Generos AS G ON FG.IdGenero = G.Id
+WHERE G.Genero = 'Mistério';
+
+
+--12
+SELECT F.Nome AS NomeDoFilme, A.PrimeiroNome, A.UltimoNome, EF.Papel
+FROM Filmes AS F
+JOIN ElencoFilme AS EF ON F.Id = EF.IdFilme
+JOIN Atores AS A ON EF.IdAtor = A.Id;
